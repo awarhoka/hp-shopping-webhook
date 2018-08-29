2ba54200de2f0cac334172261a8f66bacd058c3e
warhokar <warhokar@DESKTOP-9LLQUEK.auth.hpicorp.net>
2018-08-29 10:59:42 +0530

    added select Courosal

--- src/main/java/com/hp/shopping/impl/DialogFlowServiceImpl.java
+++ src/main/java/com/hp/shopping/impl/DialogFlowServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hp.shopping.impl;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,50 +19,103 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2IntentMessage;
 import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2IntentMessageCard;
 import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2IntentMessageCardButton;
+import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2OriginalDetectIntentRequest;
+import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2QueryResult;
 import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2WebhookRequest;
 import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2WebhookResponse;
 import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.hp.shopping.api.AppConstants;
 import com.hp.shopping.api.DialogFlowService;
+import com.hp.shopping.business.DialogFlowRequestHandler;
 /*
 @author warhokar
 */
 @RestController
-public class DialogFlowServiceImpl implements DialogFlowService {
+public class DialogFlowServiceImpl implements DialogFlowService, AppConstants{
    public static final Gson gson=new Gson(); 
+   @Autowired
+   DialogFlowRequestHandler dialogFlowRequestHandler;
 	@Override
 	public GoogleCloudDialogflowV2WebhookResponse dialogflowFulfillmentV2(HttpServletRequest request) {
 	    final HttpHeaders httpHeaders= new HttpHeaders();
 	    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-	    try {
-	        JsonObject reqJson = new JsonParser().parse(request.getReader()).getAsJsonObject();
-	        System.out.println(reqJson);
-	      }
-	      catch (Exception ex) {
-	      }
-		  GoogleCloudDialogflowV2WebhookResponse response =new GoogleCloudDialogflowV2WebhookResponse();
-				  response.setFulfillmentText("Hello Dialogflow");
-			List<GoogleCloudDialogflowV2IntentMessage> fulfillmentMessages =new ArrayList<>(); 
-			GoogleCloudDialogflowV2IntentMessage googleCloudDialogflowV2IntentMessage =new GoogleCloudDialogflowV2IntentMessage();
-				  GoogleCloudDialogflowV2IntentMessageCard card = new GoogleCloudDialogflowV2IntentMessageCard();
-				  java.util.List<GoogleCloudDialogflowV2IntentMessageCardButton> buttons =new  ArrayList<>(); 
-				  GoogleCloudDialogflowV2IntentMessageCardButton button = new  GoogleCloudDialogflowV2IntentMessageCardButton();
-				  button.setText("Test Button"); 
-				  button.setPostback("http://www.google.com");
-				  buttons.add(button);
-				  card.setTitle("This Is HP test Card"); card.setButtons(buttons);
-				  card.setImageUri("http://www.www8-hp.com/us/en/images/i_pro_02_probook_450_tcm245_2192435_tcm245_2193324_tcm245-2192435.jpg"); 
-				  card.setSubtitle("This is Subtitle");
-				  googleCloudDialogflowV2IntentMessage.setCard(card);
-				  fulfillmentMessages.add(googleCloudDialogflowV2IntentMessage);
-				  response.setFulfillmentMessages(fulfillmentMessages);
-		return response;
+	    GoogleCloudDialogflowV2WebhookRequest dialogflowV2WebhookRequestObj = this.buildDialogFlowRequest(request);
+	    return dialogFlowRequestHandler.handleDialogFlowV2Request(dialogflowV2WebhookRequestObj);
 	}
 	@Override
 	public GoogleCloudDialogflowV2WebhookResponse dialogflowFulfillmentV2v1(@RequestBody GoogleCloudDialogflowV2WebhookRequest jsonObject) {
 		System.out.println("JSON OBJECT : "+jsonObject);
 		return new GoogleCloudDialogflowV2WebhookResponse().setFulfillmentText("Hi There");
+	}
+	
+	private GoogleCloudDialogflowV2WebhookRequest buildDialogFlowRequest(HttpServletRequest request) {
+		GoogleCloudDialogflowV2WebhookRequest googleCloudDialogflowV2WebhookRequest =new GoogleCloudDialogflowV2WebhookRequest();
+		 try {
+			JsonObject reqJson = new JsonParser().parse(request.getReader()).getAsJsonObject();
+			googleCloudDialogflowV2WebhookRequest.setResponseId(reqJson.get(RESPONSEID).getAsString());
+			googleCloudDialogflowV2WebhookRequest.setSession(reqJson.get(SESSION).getAsString());
+			googleCloudDialogflowV2WebhookRequest.setQueryResult(this.getGoogleCloudDialogflowV2QueryResult(reqJson.getAsJsonObject(QUERYRESULT)));
+			googleCloudDialogflowV2WebhookRequest.setOriginalDetectIntentRequest(this.getOrigionalDetectIntentRequest(reqJson.getAsJsonObject(ORIGIONAL_DETECT_INTENT_REQUEST)));
+
+		} catch (JsonIOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return googleCloudDialogflowV2WebhookRequest;
+	}
+	private GoogleCloudDialogflowV2QueryResult getGoogleCloudDialogflowV2QueryResult(JsonObject queryResult) {
+		GoogleCloudDialogflowV2QueryResult  dialogflowV2QueryResult = new GoogleCloudDialogflowV2QueryResult();
+		dialogflowV2QueryResult.setQueryText(queryResult.get(QUERYTEXT).getAsString());
+		dialogflowV2QueryResult.setAction(queryResult.get(ACTION).getAsString());
+		dialogflowV2QueryResult.setParameters(this.getQueryResultParameters(queryResult.getAsJsonObject(PARAMETERS)));
+		dialogflowV2QueryResult.setAllRequiredParamsPresent(queryResult.get(ALL_REQUIREF_PARAMS_PRESENT).getAsBoolean());
+		dialogflowV2QueryResult.setLanguageCode(queryResult.get(LANGUAGE_CODE).getAsString());
+		dialogflowV2QueryResult.setIntentDetectionConfidence(queryResult.get(INTENT_DETECT_CONFIDENCE).getAsFloat());
+		return dialogflowV2QueryResult;
+	}
+	/**
+	 * @return Map<String, Object>
+	 * @param parameters
+	 */
+	private Map<String, Object> getQueryResultParameters(JsonObject parameters) {
+		Map<String, Object> dialogflowV2QueryResultParams =new HashMap<>();
+		Set<String> paramKeys =parameters.keySet();
+		if(paramKeys!=null &&  paramKeys.size()> 0) {
+			Iterator<String> keysItr = paramKeys.iterator();
+			while(keysItr.hasNext()) {
+				String keyStr = keysItr.next();
+				String value = parameters.get(keyStr).getAsString();
+				dialogflowV2QueryResultParams.put(keyStr, value);
+			}
+		}
+		return dialogflowV2QueryResultParams;
+	}
+	
+	/**
+	 * @param originalRequest
+	 */
+	private GoogleCloudDialogflowV2OriginalDetectIntentRequest getOrigionalDetectIntentRequest(JsonObject originalRequest) {
+		GoogleCloudDialogflowV2OriginalDetectIntentRequest originalDetectIntentRequest = new GoogleCloudDialogflowV2OriginalDetectIntentRequest();
+		if(null != originalRequest &&  originalRequest.size() > 1 ) {
+			String origionalRequestSource =originalRequest.get(SOURCE).getAsString();
+			originalDetectIntentRequest.setSource(origionalRequestSource);
+			JsonObject payload = originalRequest.getAsJsonObject(PAYLOAD);
+			Map<String, Object> origionalRequestPayload = new HashMap<>();
+			origionalRequestPayload.put(SOURCE, payload.get(SOURCE).getAsString());	
+			origionalRequestPayload.put(DATA, payload.get(DATA));
+			originalDetectIntentRequest.setPayload(origionalRequestPayload);
+		}
+		return originalDetectIntentRequest;
 	}
 	
 	 /*
